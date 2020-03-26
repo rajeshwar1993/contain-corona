e3ec7e47afb993d8d42f23bded3997b808d31d80
Ubuntu <ubuntu@ip-172-31-90-253.ec2.internal>
2020-03-26 17:27:04 +0000

    Updates

---
+++ precache-manifest.dbae65012bb830524246882110a76524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab44cbcaab779f78f7df983804a9117f",
+    "url": "/contain-corona/index.html"
+  },
+  {
+    "revision": "27af6df2537dff78d791",
+    "url": "/contain-corona/static/css/2.e15a0812.chunk.css"
+  },
+  {
+    "revision": "14d2bfc8192ecc0c5ccf",
+    "url": "/contain-corona/static/css/main.4d53b008.chunk.css"
+  },
+  {
+    "revision": "27af6df2537dff78d791",
+    "url": "/contain-corona/static/js/2.03544e28.chunk.js"
+  },
+  {
+    "revision": "14d2bfc8192ecc0c5ccf",
+    "url": "/contain-corona/static/js/main.7dad16b8.chunk.js"
+  },
+  {
+    "revision": "c7f59d5a11342be6cb38",
+    "url": "/contain-corona/static/js/runtime~main.2d1579d7.js"
+  }
+]);
